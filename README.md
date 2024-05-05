d8f6152269973bb6b168c48bc4430f28a1ebc26e
steven-fo <stevenfo0711@gmail.com>
2024-05-05 23:24:02 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## 1.2 Understanding how it works
 ![](images/1.2.png)
 `Steven's Komputer: hey hey` came out first because it is ran outside the async function. The following 2 lines are priinted after the `hey hey` because they are inside the async function.
+
+## 1.3 Multiple Spawn and removing drop
+![](images/1.3.png)
+It can be seen from the output that `hey hey` still come out first, then followed by `howdy and done`. The first function will be run, which returns howdy. Then the second function will be run, which will return howdy2. At last, the third function will be run, which will return howdy3. Remember the program's sequence, where the function are run according to their order, from 1 to 3. Therefore, after howdy3, the program will give done, done2, and at last done3.
--- src/main.rs
+++ src/main.rs
@@ -105,11 +105,23 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Steven's Komputer: done!");
     });
+    spawner.spawn(async {
+        println!("Steven's Komputer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Steven's Komputer: done2!");
+    });
+    spawner.spawn(async {
+        println!("Steven's Komputer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Steven's Komputer: done3!");
+    });
 
     println!("Steven's Komputer: hey hey");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
